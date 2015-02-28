038e4ea3362a843f1b2852fbd2c2d364aaccddd3
javier abanses <javier.abanses@gmail.com>
2015-02-28 13:42:33 +0000

    initial

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+
+var gulp = require('gulp'),
+    requireDir = require('require-dir');
+
+//load gulp tasks from gulp directory
+requireDir('./gulp')
+requireDir('./gulp/generators')
+requireDir('./gulp/tasks')
+requireDir('./gulp/workflows')
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * workflows
+ * */
+
+gulp.task('build', ['html', 'scripts', 'less', 'images', 'data']);
+gulp.task('build-angular', ['html', 'scripts','ng-copy-templates' ,'less', 'images', 'data']);
+
+/*
+ * default
+ * */
+
+gulp.task('default-no-browser-sync', ['build', 'watch', 'server']);
+gulp.task('default', ['build', 'watch', 'server-with-browser-sync']);
+
+gulp.task('default-angular', ['build-angular', 'watch-angular', 'server-with-browser-sync']);
+gulp.task('default-angular-no-browser-sync', ['build-angular', 'watch-angular', 'server-with-browser-sync']);
