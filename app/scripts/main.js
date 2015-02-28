1df1aa9120abc55d6bd0e51297826907d07f98ff
javier abanses <javier.abanses@gmail.com>
2015-02-28 15:29:20 +0000

    updates and dist

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,9 +7,13 @@
 var d3 = require("d3-browserify"),
     $ = require("jQuery")
 
+window.d3 = d3;
+window.$ = $;
 
-d3.json("data/data.json", function(error, json) {
-    if (error) return console.warn(error);
-    console.log( '::() json = ' , json );
-});
 
+//
+//d3.json("data/data.json", function(error, json) {
+//    if (error) return console.warn(error);
+//    console.log( '::() json = ' , json );
+//});
+//
